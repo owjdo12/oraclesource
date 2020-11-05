aeb3b694fab370b4941bc1d54d31d153b4b5b162
yeji <owjdo12@gmail.com>
2020-11-05 18:26:01 +0900

    oracle 함수

--- hr1104.sql
+++ hr1104.sql
@@ -66,10 +66,10 @@ SELECT department_id, first_name, job_id, SALARY * COMMISSION_PCT AS commission 
 
 -- [실습]문자열 함수
 -- 1.first_name이 Curtis인 사람의 first_name, last_name, email, phone_number, job_id를 조회한다. 단 job_id의 결과는 소문자로 출력되도록
-SELECT first_name, last_name, email, phone_number, LOWER(job_id) FROM emp WHERE first_name = 'Curtis';
+SELECT first_name, last_name, email, phone_number, LOWER(job_id) FROM employees WHERE first_name = 'Curtis';
 
 -- 2. 부서번호가 70,80,90인 사람들의 employee_id, first_name, hire_date, job_id를 조회한다. 단, job_id가 it_prog인 사원의 경우 프로그래머로 변경하여 출력한다.
-SELECT employee_id, first_name, hire_date, REPLACE(job_id, 'IT_PROG', '프로그래머') FROM emp WHERE department_id IN (70, 80, 90);
+SELECT employee_id, first_name, hire_date, REPLACE(job_id, 'IT_PROG', '프로그래머') FROM employees WHERE department_id IN (60, 70, 80, 90);
 
 -- 3. job_id가 ad_pres, pu_clerk인 사원들의 employee_id, first_name, last_name, department-id, job_id를 조회하시오. 단, 사원명은 first_name과 last_name을 연결하여 출력한다.
-
+SELECT employee_id, CONCAT(first_name,CONCAT(' ',last_name)) department_id, job_id FROM employees WHERE job_id IN('ad_pres', 'pu_clerk');
